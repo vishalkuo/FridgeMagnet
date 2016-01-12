61a90249be2d2e9e2d8e3b6d5b99b5c834dea27c
vishalkuo <vishalkuo@hotmail.com>
2016-01-11 22:32:47 -0500

    hidden char comm

--- Hardware/ArduinoPiCommTest/Pi2Arduino.py
+++ Hardware/ArduinoPiCommTest/Pi2Arduino.py
@@ -3,9 +3,8 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 ser.write('5')
 while 1:
     line = ser.readline()
-    print(line)
-    if line == 'Done!':
-	print line
+    if line == "1\r\n":
 	break
+
 
 print("End program")
