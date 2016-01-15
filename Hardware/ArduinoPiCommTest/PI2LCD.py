6facdccbe90c6cf4830d14dea6fac71f55a4862a
vishalkuo <vishalkuo@hotmail.com>
2016-01-14 22:45:55 -0500

    Successful pi 2 lcd via arduino serial

--- Hardware/ArduinoPiCommTest/PI2LCD.py
+++ Hardware/ArduinoPiCommTest/PI2LCD.py
@@ -1,10 +1,21 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.write('5')
-while 1:
-    line = ser.readline()
-    if line == "1\r\n":
-	break
+import time
 
-ser.close()
-print("End program")
+ser = serial.Serial('/dev/ttyACM0', 9600)
+line_buffer_wait = 1
+
+def hold():
+    time.sleep(line_buffer_wait)
+
+def clear_LCD():
+    ser.write("0" + "x")
+
+def write_to_LCD(line_1, line_2):
+    clear_LCD()
+    hold()
+    ser.write("0" + line_1)
+    hold()
+    ser.write("1" + line_2)
+
+write_to_LCD("Hello", "World")
+    
